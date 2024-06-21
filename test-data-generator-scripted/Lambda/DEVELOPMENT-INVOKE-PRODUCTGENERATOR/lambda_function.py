6336c65b697aa5beeeebe72753cd243d4d4855f2
drinese18 <48971383+drinese18@users.noreply.github.com>
2024-06-21 16:17:25 -0400

    Update lambda_function.py
    Removed commented out code.

--- test-data-generator-scripted/Lambda/DEVELOPMENT-INVOKE-PRODUCTGENERATOR/lambda_function.py
+++ test-data-generator-scripted/Lambda/DEVELOPMENT-INVOKE-PRODUCTGENERATOR/lambda_function.py
@@ -12,14 +12,7 @@ glueJobName = "test-data-generator-scripted"
 
 def lambda_handler(event, context):
     logger.info('## INITIATED BY EVENT: ')
-    #logger.info(event['detail'])
     response = client.start_job_run(JobName = glueJobName)
     logger.info('## STARTED GLUE JOB: ' + glueJobName)
     logger.info('## GLUE JOB RUN ID: ' + response['JobRunId'])
     return response
-    
-    
-    #return {
-    #    'statusCode': 200,
-    #    'body': json.dumps('Hello from Lambda!')
-    #}
